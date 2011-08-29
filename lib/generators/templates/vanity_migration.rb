ab8d9cef3232bf6cff4810de13d96e05ce398129
Ariel Salomon <asalomon@mylookout.com>
2011-08-29 12:16:36 -0700

    Update migration template to fix index issues

--- lib/generators/templates/vanity_migration.rb
+++ lib/generators/templates/vanity_migration.rb
@@ -23,24 +23,23 @@ class VanityMigration < ActiveRecord::Migration
     add_index :vanity_experiments, [:experiment_id]
 
     create_table :vanity_conversions do |t|
-      t.integer :vanity_experiment_id
+      t.references :vanity_experiment
       t.integer :alternative
       t.integer :conversions
     end
     add_index :vanity_conversions, [:vanity_experiment_id, :alternative], :name => "by_experiment_id_and_alternative"
 
     create_table :vanity_participants do |t|
-      t.string :experiment_id
-      t.string :identity
+      t.references :vanity_experiment
+      t.integer :identity
       t.integer :shown
       t.integer :seen
       t.integer :converted
     end
-    add_index :vanity_participants, [:experiment_id]
-    add_index :vanity_participants, [:experiment_id, :identity], :name => "by_experiment_id_and_identity"
-    add_index :vanity_participants, [:experiment_id, :shown], :name => "by_experiment_id_and_shown"
-    add_index :vanity_participants, [:experiment_id, :seen], :name => "by_experiment_id_and_seen"
-    add_index :vanity_participants, [:experiment_id, :converted], :name => "by_experiment_id_and_converted"
+    add_index :vanity_participants, [:vanity_experiment_id, :identity], :name => "by_experiment_id_and_identity"
+    add_index :vanity_participants, [:vanity_experiment_id, :shown], :name => "by_experiment_id_and_shown"
+    add_index :vanity_participants, [:vanity_experiment_id, :seen], :name => "by_experiment_id_and_seen"
+    add_index :vanity_participants, [:vanity_experiment_id, :converted], :name => "by_experiment_id_and_converted"
   end
 
   def self.down
